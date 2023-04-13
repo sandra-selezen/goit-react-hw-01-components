e1c1a9bcb3f059a6b42925a1244bcc2a5da863f7
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-04-13 14:25:37 +0300

    Update Profile component

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,6 @@ import user from "user.json";
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats} />
-    </div>
+      <Profile user={user} />
   );
 };
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-export const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => {
+export const Profile = ({ user: { username, tag, location, avatar, stats: { followers, views, likes } } }) => {
   return (<div className="profile">
     <div className="description">
       <img
@@ -31,11 +31,15 @@ export const Profile = ({ username, tag, location, avatar, stats: { followers, v
 }
 
 Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired
+  }).isRequired
 }
