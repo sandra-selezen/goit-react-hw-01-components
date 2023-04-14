4f533cf77a00f5a9ed65c45f3fc0744a37e6e5ca
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-04-14 14:02:39 +0300

    Update styles for FriendListItem component

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -4,7 +4,7 @@ import css from "./FriendList.module.css";
 export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
   return (
     <>
-      <span className={css.status}>{isOnline}</span>
+      {isOnline ? <span className={css.isOnline}></span> : <span className={css.isOffline}></span>}
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </>
